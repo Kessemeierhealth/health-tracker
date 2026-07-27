c74dcc0b4f6ee5714a51162c5a8c2a67fd88ac14
Kessemeier <goetz.kessemeier@sogeti.com>
2026-07-27 18:10:11 +0200

    M1.3 Gesundheitsdaten und individuelle Dashboards vorbereitet

---
+++ lib/models/health_data.dart
@@ -0,0 +1,21 @@
+class HealthData {
+    double? weight;
+    double? waistCircumference;
+    double? bodyFatPercentage;
+
+    int? systolic;
+    int? diastolic;
+    int? pulse;
+
+    int? steps;
+
+    HealthData({
+        this.weight,
+        this.waistCircumference,
+        this.bodyFatPercentage,
+        this.systolic,
+        this.diastolic,
+        this.pulse,
+        this.steps,
+    });
+}
